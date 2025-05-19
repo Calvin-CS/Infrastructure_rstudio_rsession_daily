ada3019b6c539948191bc10e75e43a946572745c
Chris Wieringa <cwieri39@calvin.edu>
2025-05-19 08:52:33 -0400

    update to URL for passwd/group and disable cron runs

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,16 @@ SHELL ["/bin/bash", "-c"]
 
 # Mount the HTPASSWD secret so that we can pull down the current
 # passwd and group files for inclusion into the container
-RUN --mount=type=secret,id=HTPASSWD_CSWEB_WGET \
-    cat /run/secrets/HTPASSWD_CSWEB_WGET | /usr/bin/base64 -d > /root/.wgetrc
+
+RUN --mount=type=secret,id=CS_POPULATE_SERVER \
+    cat /run/secrets/HTPASSWD_CSWEB_WGET > /tmp/cspopulate-url.txt
 COPY --chmod=0755 inc/cs-populate-users.sh /root
 RUN /root/cs-populate-users.sh && \
-    rm -f /root/.wgetrc
+    rm -f /tmp/cspopulate-url.txt
+
+
+# RUN --mount=type=secret,id=HTPASSWD_CSWEB_WGET \
+#     cat /run/secrets/HTPASSWD_CSWEB_WGET | /usr/bin/base64 -d > /root/.wgetrc
+# COPY --chmod=0755 inc/cs-populate-users.sh /root
+# RUN /root/cs-populate-users.sh && \
+#     rm -f /root/.wgetrc
--- inc/cs-populate-users.sh
+++ inc/cs-populate-users.sh
@@ -1,37 +1,13 @@
+#!/bin/bash
+
 # use wget to pull down PASSWD/GROUP entries using secret password
-if test -f "/root/.wgetrc"; then
-	echo "Found the HTPASSWD CSWEB secret... proceeding"
+if test -f "/tmp/cspopulate-url.txt"; then
+        echo "Found the CSPOPULATE URL secret... proceeding"
 
-	# pull down the file
-	cd /root/
-	wget https://r.cs.calvin.edu/.well-known/cs/cspasswd
-	wget https://r.cs.calvin.edu/.well-known/cs/csgroup
-	wget https://r.cs.calvin.edu/.well-known/cs/csusergroups
+        URL=$(cat /tmp/cspopulate-url.txt)
 
-	# test to make sure they exist; if they do copy them into passwd/group
-	if test -f "/root/cspasswd"; then
-		echo "Populating /etc/passwd with CS entries"
-		cat /root/cspasswd >> /etc/passwd
-	else
-		echo "*** CSPASSWD NOT FOUND ***"
-		exit 1
-	fi
-
-	if test -f "/root/csgroup"; then
-		echo "Populating /etc/group with CS entries"
-		cat /root/csgroup >> /etc/group
-	else
-		echo "*** CSGROUP NOT FOUND ***"
-		exit 1
-	fi
-
-	if test -f "/root/csusergroups"; then
-		echo "Populating /etc/group with CS per-user entries"
-		cat /root/csusergroups >> /etc/group
-	else
-		echo "*** CSUSERGROUPS NOT FOUND ***"
-		exit 1
-	fi
+        curl -fsSLk https://${URL}/csallusers >> /etc/passwd
+    	curl -fsSLk https://${URL}/csallgroups >> /etc/group
 
 # error out
 else
